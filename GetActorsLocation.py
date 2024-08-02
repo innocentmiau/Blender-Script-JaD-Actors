c92d8fbd9cf8ecd54c107f6823d30c6416dc0c54
Miau <40339453+innocentmiau@users.noreply.github.com>
2024-08-02 04:18:04 +0100

    Update GetActorsLocation.py

--- GetActorsLocation.py
+++ GetActorsLocation.py
@@ -11,7 +11,10 @@ actors = {
     "eco-red": [],
     "ropebridge": [],
     "crate": [],
-    "plat-eco": []
+    "plat-eco": [],
+    "swingpole": [],
+    "orb-cache-top": [],
+    "ecovent": []
 }
 
 heightAdded = {
@@ -22,7 +25,10 @@ heightAdded = {
     "eco-red": 0,
     "ropebridge": 0,
     "crate": 0,
-    "plat-eco": 0
+    "plat-eco": 0,
+    "swingpole": 0,
+    "orb-cache-top": -0.67,
+    "ecovent": 1
 }
 
 contentTypes = {
@@ -53,12 +59,30 @@ def transform_quat(quat):
     w, x, z, y = quat
     return [x, y, -z, w]
 
+def transform_quat_swingpole(quat):
+    w, x, z, y = quat
+    return [x, y, z, w]
+
+def list_custom_properties(obj):
+    properties = {}
+    for key in obj.keys():
+        if key not in "_RNA_UI":
+            properties[key] = obj[key]
+    return properties
+
+def turnOffActorForLevel(obj):
+    obj["set_invisible"] = True
+    obj["set_collision"] = True
+    obj["ignore"] = True
+
 objectsToUseAsReference = {}
 for obj in bpy.context.scene.objects:
     if "-final" not in obj.name:
         continue
     location = transform_location([round(coord, 3) for coord in obj.location])
     objectsToUseAsReference.update({obj.name: location})
+    custom_props = list_custom_properties(obj)
+    turnOffActorForLevel(obj)
 
 def collect_object_locations():
     for collection_name in bpy.data.collections.keys():
@@ -67,7 +91,10 @@ def collect_object_locations():
             for obj in collection.objects:
                 location = transform_location([round(coord, 3) for coord in obj.location])
                 quat = transform_quat([round(coord, 3) for coord in obj.rotation_quaternion])
+                if collection_name == "swingpole":
+                    quat = transform_quat_swingpole([round(coord, 3) for coord in obj.rotation_quaternion])
                 actors[collection_name].append((location, quat, obj.name))
+                turnOffActorForLevel(obj)
 
 def generate_text():
     lines = []
@@ -75,12 +102,40 @@ def generate_text():
         for count, (location, quat, name) in enumerate(objects, start=1):
             
             etype = "crate" if actor_type == "buzzer" else actor_type
+            if actor_type == "money":
+                quat = [0.0, 0.0, 0.0, 1.0]
             
+            height_to_add = heightAdded[actor_type]
             lump_extra = ""
             if actor_type == "buzzer":
                 lump_extra = f""",
         "crate-type": "'iron",
         "eco-info": ["int32", 8, 1]"""
+            if actor_type == "ecovent":
+                ecotype = 3
+                if "green" in name:
+                    ecotype = 4
+                if "red" in name:
+                    ecotype = 2
+                if "yellow" in name:
+                    ecotype = 1
+                lump_extra = f""",
+        "eco-info": ["int32", {ecotype}, 1]"""
+            if actor_type == "orb-cache-top":
+                orbsAmount = 10
+                if "orbs" in name:
+                    start_index = name.index("orbs") + len("orbs")
+                    number = name[start_index:]
+                    numb = ""
+                    for char in number:
+                        if char.isdigit():
+                            numb += char
+                        else:
+                            break
+                        if numb.isnumeric():
+                            orbsAmount = int(numb)
+                lump_extra = f""",
+        "orb-cache-count": ["int32", {orbsAmount}]"""
             if actor_type == "crate":
                 crateType = "wood"
                 for crate_key in crateTypes:
@@ -112,18 +167,18 @@ def generate_text():
                 if "ropebridge-70" in name:
                     art_name = "ropebridge-70"
                 if art_name != None:
-                    heightAdded[actor_type] = -2
+                    height_to_add = -2
                     lump_extra = f""",
         "art-name": "{art_name}" """
 
-            coords = [location[0], (location[1] + heightAdded[actor_type]), location[2]]
+            coords = [location[0], round((location[1] + height_to_add), 3), location[2]]
             coords2 = coords.copy()
             coords2.append(10)
 
             if actor_type == "plat-eco":
                 pathCoords = coords.copy()
                 pathCoords.append(1.0)
-                pathCoordsFinal = coords.copy()
+                pathCoordsFinal = pathCoords.copy()
                 reference = name + "-final"
                 velocity = 10
                 for obj in objectsToUseAsReference.keys():
